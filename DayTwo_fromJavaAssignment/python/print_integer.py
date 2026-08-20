cecf3924b3232ac6d7712bbda2010f05ea31a09c
Fayokemi22 <fayokemioluwatunminininu@gmail.com>
2026-08-21 00:31:15 +0100

    Add DayTwo folder from JavaAssignment

---
+++ DayTwo_fromJavaAssignment/python/print_integer.py
@@ -0,0 +1,25 @@
+for count in range(100):
+    if (count%3!=0): 
+        print("helloworld")
+
+    if (count%5!=0):
+        print("helloworld")
+        
+    if (count%7!=0):
+        print("helloworld") 
+              
+    if (count%3==0):
+        print("hello/hello/orion")
+    
+    
+    if (count%5==0):
+        print("hello/hello/orion")
+    
+    
+    if (count%7==0):
+       print("hello/hello/orion")
+    
+    
+       print("");
+    
+    
